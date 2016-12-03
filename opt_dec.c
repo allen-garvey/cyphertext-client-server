a21900bc05f65807a111cb00b61f0d01966b1f3d
Allen Garvey <allen.garvey@gmail.com>
2016-12-02 23:55:43 -0500

    Add comments to decode client

--- opt_dec.c
+++ opt_dec.c
@@ -1,2 +1,14 @@
+/* 
+ * Client for decoding text using one time pad
+ * by: Allen Garvey
+ * usage: opt_dec <plaintext_file> <key_file> <port>
+ */
+
+//the only difference between the encode and decode clients
+//is the identification message initially sent to the server
+//so we just have to define the constant and include the code
+//for the encode client
+
+//used in initial message to server to identify client
 #define CLIENT_IDENTIFICATION_HEADER "DECODE\n"
 #include "opt_enc.c"
