754945fef51bef397f2da6ec1fcd9ada536423bc
Allen Garvey <allen.garvey@gmail.com>
2016-12-02 20:53:02 -0500

    Simplify constant definitions in decoder server

--- opt_dec_d.c
+++ opt_dec_d.c
@@ -10,15 +10,11 @@
 */
 //string used to represent client is the correct one
 //should be sent as first message from client
-#ifndef ACCEPTED_MESSAGE_HEADER
 #define ACCEPTED_MESSAGE_HEADER "DECODE\n"
 //number of chars including null char in header
 #define ACCEPTED_MESSAGE_HEADER_LENGTH 8
-#endif
 
-#ifndef CHARACTER_TRANSFORMATION_FUNCTION_POINTER
 #define CHARACTER_TRANSFORMATION_FUNCTION_POINTER &decodeCharacter 
-#endif
 
 //with the exception of the above constants, encoding and decoding server should work the same
 //so just include the code for the encoding server
